283163dab7440e535e70415c4947a29fe63738c5
Anna <127552050+mercurybased@users.noreply.github.com>
2023-05-09 14:57:28 -0700

    fixed socket.io

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const session = require('express-session');
-<<<<<<< HEAD
 const http = require('http');
-=======
 const routes = require('./controllers');
-
->>>>>>> dev
+const path = require('path')
 const exphbs = require('express-handlebars');
-
 const sequelize = require('./config/connection');
-
-// Import the custom helper methods
-// const helpers = require('./utils/helpers');
-const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 const sess = {
   secret: "Super secret secret",
   cookie: {},
@@ -29,52 +17,19 @@ const sess = {
     db: sequelize
   })
 };
-
-
-
-
 const server = http.createServer(app)
 const io = require('socket.io')(server);
 app.use(session(sess));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.use(express.static("public"))
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => 
-{
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
-}
-);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
-
 app.use(routes);
-=======
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-
-
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
->>>>>>> dev
-
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
@@ -82,26 +37,6 @@ io.on('connection', (socket) => {
 });
 sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, () => console.log('Now listening'));
+}).catch (err => {
+  console.log(err)
 });
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-
-
-app.listen(PORT, () => {
-  console.log('Now listening')
-  sequelize.sync({ force: false })
-});
->>>>>>> dev
