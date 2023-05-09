87671464d471125ef4491918c9486e4a901e8be0
Anna <127552050+mercurybased@users.noreply.github.com>
2023-05-09 11:22:45 -0700

    deleted bio route, added into user model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Interest = require('./Interest');
-const Biography = require('./Biography');
+// const Biography = require('./Biography');
 // const Location = require('./Location');
 // const Image = require('./Image');
 
@@ -12,13 +12,13 @@ Interest.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasOne(Biography)
-Biography.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// User.hasOne(Biography)
+// Biography.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
 // User.hasOne(Location)
 
 // User.hasOne(Image)
 
-module.exports = { User, Interest, Biography};
+module.exports = { User, Interest };
