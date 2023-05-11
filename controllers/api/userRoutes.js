d901d7ca1ba127fce27d36b915e302b6cdd1351f
Anna <127552050+mercurybased@users.noreply.github.com>
2023-05-11 10:45:20 -0700

    fixed login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -79,12 +79,10 @@ router.post("/login", async (req, res) => {
         .json({ message: "Incorrect email or password, please try again" });
 
       return;
-    }
-
-    req.session.save(() => {
+    }    
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-    });
+      
     res.json({ user: userData, message: "You are now logged in!" });
   } catch (err) {
     res.status(400).json(err);
