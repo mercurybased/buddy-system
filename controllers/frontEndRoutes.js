58962dbef1fcecc96f359b7d7c2413ebe19923e3
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-11 13:00:55 -0700

    added a log out page

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -7,25 +7,25 @@ router.get("/", (req, res) => {
         logged_in: req.session.logged_in
     })
 })
-    // Post.findAll({
-    //     include: [User]
-    // }).then(postData => {
-    //     const hbsData = postData.map(post => post.get({ plain: true }));
-    //     console.log(hbsData);
-    //     res.render("home", {
-    //         allPosts: hbsData,
-    //         logged_in: req.session.logged_in
-    //     })
-    // })
+// Post.findAll({
+//     include: [User]
+// }).then(postData => {
+//     const hbsData = postData.map(post => post.get({ plain: true }));
+//     console.log(hbsData);
+//     res.render("home", {
+//         allPosts: hbsData,
+//         logged_in: req.session.logged_in
+//     })
+// })
 // })
 
-router.get("/chat" ,(req,res)=>{
-    res.render("chat" ,{
-        logged_in:req.session.logged_in     
+router.get("/chat", (req, res) => {
+    res.render("chat", {
+        logged_in: req.session.logged_in
     })
 })
-router.get("/login",(req,res)=>{
-    if(req.session.logged_in){
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
         return res.redirect("/profile")
     }
     res.render("login", {
@@ -44,34 +44,37 @@ router.get("/search", (req, res) => {
 })
 
 router.get("/profile", async (req, res) => {
-        console.log(req.session)
-    if(!req.session.logged_in){
+    console.log(req.session)
+    if (!req.session.logged_in) {
         return res.redirect("/login")
     }
     // check who is logged in from req.session
     const userId = req.session.user_id
     // find that user (and all of their data)
-    const userData= await User.findByPk(userId, {
-        include: {all:true, nested:true}
+    const userData = await User.findByPk(userId, {
+        include: { all: true, nested: true }
     })
-    const user = userData.get({plain: true})
+    const user = userData.get({ plain: true })
     console.log(user)
     // render profile, and send the userData
     res.render("profile", {
-       userData:user
+        userData: user
     })
 })
 
 // 
-router.get('/logout', (req,res) => {
+router.get('/logout', (req, res) => {
     console.log("loggin out!!")
     try {
-      req.session.destroy()
-      res.status(200).json({msg:"logged out successfully"})
+        res.render("logout", {
+            logged_in: req.session.logged_in
+        })
+        req.session.destroy()
+        res.status(200).json({ msg: "logged out successfully" })
     } catch (error) {
-      console.log(error)
-      res.status(500).json(error)
+        console.log(error)
+        res.status(500).json(error)
     }
-  })
+})
 
 module.exports = router;
