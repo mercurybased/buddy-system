cc4d93e89e61009b95623134d456a91e7339b0b8
Anna <127552050+mercurybased@users.noreply.github.com>
2023-05-12 13:14:39 -0700

    change profile photo

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,5 @@
+let url = '' //"whatever.jpg"
+
 document.querySelector("form").addEventListener("submit",e=>{
     e.preventDefault();
     const userInterest = {
@@ -35,3 +37,40 @@ allDelBtns.forEach(button=>{
     })
 })
 
+const form = document.querySelector("form");
+
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'de19jsefk',
+    uploadPreset: 'wk5eacxt'
+  },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log('Done! Here is the image info: ', result.info);
+        url = result.info.url
+        console.log(url)
+        changePhoto()
+    }
+}
+)
+
+const changePhoto = async () => {
+     if (true) {
+        const response = await fetch ("/api/users/profile", {
+            method: "PUT",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify ({
+                photoUrl:url
+            })
+        })
+        console.log(response);
+    if (response.status === 200) {
+      window.location.assign("/profile")
+    } else {
+      alert("please choose a photo")
+    }
+     }
+}
+
+  document.getElementById("upload_widget").addEventListener("click", function () {
+    myWidget.open();
+  }, false);
