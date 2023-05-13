dbe660ac5c0f93b6f38663fc69cbc20291f84a1f
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-05-13 02:45:09 -0700

    not sure if I pushed

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sess = {
   secret: "Super secret secret",
-  cookie: { maxAge: 1000*60*60*3,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 3,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
@@ -33,7 +34,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -44,24 +45,24 @@ app.get('/', (req, res) => {
 });
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
-    
+
     //object with message inside of it with an id
     //io.to(user1.id).to(user2.id).emit('startChat', [user1.userId, user2.userId]);
-   
+
     //if statement where you emit to certain person if there ids paired
     //if (user1==user2) {
-     // console.log('Message:',);
+    // console.log('Message:',);
     ///} else {
-   //
+    //
     //save messages to models
-    
-    
-    
+
+
+
     io.emit('chat message', msg);
   });
 });
 sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, () => console.log('Now listening'));
-}).catch (err => {
+}).catch(err => {
   console.log(err)
 });
